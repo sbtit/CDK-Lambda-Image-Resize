7aa0a5d53cd598ac9b0fa997fd2e7336c7f24256
Shibata-Da <40657033+sbtit@users.noreply.github.com>
2024-03-28 13:33:14 +0900

    Initial commit

---
+++ test/cdk-lambda-image-resize.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdaImageResize from '../lib/cdk-lambda-image-resize-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lambda-image-resize-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdaImageResize.CdkLambdaImageResizeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
